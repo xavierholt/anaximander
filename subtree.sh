55efc92601026065b560edc10aabe9d6f7699ee3
Kevin Burk <xavierholt@gmail.com>
2017-11-01 22:58:07 -0700

    Point the KD subtree at the wrap-nodes branch.

--- subtree.sh
+++ subtree.sh
@@ -1,2 +1,2 @@
 # See https://www.atlassian.com/blog/git/alternatives-to-git-submodule-git-subtree
-git subtree pull --prefix src/gen/kd https://github.com/xavierholt/kd.git master --squash
+git subtree pull --prefix src/gen/kd https://github.com/xavierholt/kd.git wrap-nodes --squash
